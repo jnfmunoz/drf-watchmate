a4bec23f5c169171faaf04cc337d8883fb3eb190
juan <juanmunozgrau@gmail.com>
2023-07-22 20:04:04 -0400

    add-practice 3

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,33 +1,85 @@
 from rest_framework import serializers
-from watchlist_app.models import Movie
+from watchlist_app.models import WatchList, StreamPlatform, Review
 
 ##ModelSerializer
-class MovieSerializer(serializers.ModelSerializer):
-    #Custom Serializer Fields
-    len_name = serializers.SerializerMethodField()
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = Review
+        exclude = ['watchlist']
+        # fields = "__all__"
+
+class WatchListSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
-        model = Movie
+        model = WatchList
         fields = "__all__"
-        # fields = ['id', 'name', 'description']
-        # exclude= ['active']
+
+class StreamPlatformSerializer(serializers.HyperlinkedModelSerializer):
+    #Nested serializers
+    watchlist = WatchListSerializer(many=True, read_only=True)
     
-    def get_len_name(self, object):
-        return len(object.name)
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
 
-    #Object-level validation
-    def validate(self, data):
-        if data['name'] == data['description']:
-            raise serializers.ValidationError("Title and Description should be different!")
-        else:
-            return data
+# class StreamPlatformSerializer(serializers.ModelSerializer):
+#     #Nested serializers
+#     watchlist = WatchListSerializer(many=True, read_only=True)
+    
+#     class Meta:
+#         model = StreamPlatform
+#         fields = "__all__"
 
-    #Field-level validation
-    def validate_name(self, value):
-        if len(value) < 2:
-            raise serializers.ValidationError("Name is too short!")
-        else:
-            return value
+    #Serializer relations
+    #StringRelatedField
+    # watchlist = serializers.StringRelatedField(many=True)
+    
+    #PrimaryKeyRelatedField
+    # watchlist = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    #HyperlinkedRelatedField
+    # watchlist = serializers.HyperlinkedRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     view_name='movie-detail'
+    # )
+
+
+    
+
+ 
+    
+
+
+
+
+
+#Validation
+
+    # fields = ['id', 'name', 'description']
+    # exclude= ['active']
+
+    #Custom Serializer Fields
+    #len_name = serializers.SerializerMethodField()
+
+    # def get_len_name(self, object):
+    #     return len(object.name)
+
+    # #Object-level validation
+    # def validate(self, data):
+    #     if data['name'] == data['description']:
+    #         raise serializers.ValidationError("Title and Description should be different!")
+    #     else:
+    #         return data
+
+    # #Field-level validation
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short!")
+    #     else:
+    #         return value
 
 ##Serializer
 # #Validators
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -2,37 +2,88 @@ from rest_framework.response import Response
 from rest_framework import status
 #from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from watchlist_app.models import Movie
-from watchlist_app.api.serializers import MovieSerializer
+from rest_framework import generics
+#from rest_framework import mixins
 
-class MovieListAV(APIView):
-    
+from watchlist_app.models import WatchList, StreamPlatform, Review
+from watchlist_app.api.serializers import (WatchListSerializer, StreamPlatformSerializer, 
+                                            ReviewSerializer)
+
+#Concrete View Classes
+class ReviewCreate(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        watchlist = WatchList.objects.get(pk=pk)
+
+        serializer.save(watchlist = watchlist)
+
+#class ReviewList(generics.ListCreate APIView):
+class ReviewList(generics.ListAPIView):
+    #queryset = Review.objects.all()    
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Review.objects.filter(watchlist=pk)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()    
+    serializer_class = ReviewSerializer
+
+#Mixins
+# class ReviewDetail(mixins.RetrieveModelMixin, 
+#                     generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+# class ReviewList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+#APIView
+class StreamPlatformAV(APIView):
+
     def get(self, request):
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
+        platform = StreamPlatform.objects.all()
+        #serializer = StreamPlatformSerializer(platform, many=True, context={'request': request})
+        serializer = StreamPlatformSerializer(platform, many=True, context={'request': request})
         return Response(serializer.data)
-
+    
     def post(self, request):
-        serializer = MovieSerializer(data=request.data)
+        serializer = StreamPlatformSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
 
-class MovieDetailAV(APIView):
+class StreamPlatformDetailAV(APIView):
     
     def get(self, request, pk):
         try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response({'error':'Movie does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)   
+            stream = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+            return Response({'error':'Stream Platform does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        # serializer = StreamPlatformSerializer(stream)
+        serializer = StreamPlatformSerializer(stream, context={'request':request})
+        return Response(serializer.data) 
 
     def put(self, request, pk):
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie,data=request.data)
+        stream = StreamPlatform.objects.get(pk=pk)
+        serializer = StreamPlatformSerializer(stream,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -40,7 +91,46 @@ class MovieDetailAV(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
     def delete(self, request, pk):
-        movie = Movie.objects.get(pk=pk)
+        stream = StreamPlatform.objects.get(pk=pk)
+        stream.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class WatchListAV(APIView):
+    
+    def get(self, request):
+        movies = WatchList.objects.all()
+        serializer = WatchListSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = WatchListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+class WatchDetailAV(APIView):
+    
+    def get(self, request, pk):
+        try:
+            movie = WatchList.objects.get(pk=pk)
+        except WatchList.DoesNotExist:
+            return Response({'error':'Movie does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = WatchListSerializer(movie)
+        return Response(serializer.data)   
+
+    def put(self, request, pk):
+        movie = WatchList.objects.get(pk=pk)
+        serializer = WatchListSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+        movie = WatchList.objects.get(pk=pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
